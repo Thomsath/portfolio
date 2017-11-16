9ee5298afc38abcf40d42a68bab5373f7dae0afd
Thomas <thomas.beaupertuis@hotmail.fr>
2017-11-16 13:46:20 +0100

    Adding all the files

---
+++ js/app.js
@@ -0,0 +1,111 @@
+
+
+// FR TO EN
+
+$(document).ready(function() {
+
+$('#translate').click(function() {
+	var langue = $('#translate').text();
+
+	if(langue == 'EN') {
+
+	// NAVBAR
+	$('#about').text('About me');
+	$('#creas').text('Works and creations');
+	$('#contact').text('Contact me');
+	$('#translate').text('FR');
+
+
+	// Header
+
+	// HEADER
+
+	$('.big-title h4').text('FRONT-END DEVELOPMENT • GRAPH. CREATIONS');
+
+	//A PROPOS
+
+	$('.container-qui h4').html('<h4>I am <span class="myname">Thomas Beaupertuis</span>, passionate about the web for some years now. The web integration and the graphic creation are the stuff that I master the best, and that is what I like a lot. I appreciate simple things, that is why this portfolio is so simple.<br> From <span class="tlse">Toulouse</span> to <span class="bdx">Bordeaux</span>, I aquired some skills and realized some projects. Outside the web, I like a lot photography, sports and skateboarding. Scroll to know a little more.</h4>')
+
+	// CREAS
+
+	$('#sitewebs').text('Websites');
+	$('#creagraphs').text('Creations of all kinds');
+
+
+	// Snapchat
+
+	$('.container-f-snap-head h1').text('Snapchat filters');
+	$('.container-f-snap-head p').html('<p>I realized some snapchat filters in order to improve my skills on Photoshop while incrementing the content for the community of this social network.<br>It also allows me to share my creations to a public. </p>')
+
+	$('.colonne h1').text("Column to the Girondins");
+	$('.colonne p').text('Snapchat filter realized on Photoshop for the monument Column of the Girondins (Bordeaux, France) ');
+
+	$('.miroir h1').text("Water mirror");
+	$('.miroir p').text('Snapchat filter realized on Photoshop for the popular very frequented the water mirror (Bordeaux, France) ');
+
+	$('.quais h1').text("Quays of Bordeaux");
+	$('.quais p').text('Snapchat filter realized on Photoshop for the quays of Bordeaux, place very frequented by the tourist and inhabitants (Bordeaux, France) ');
+
+	$('.university h1').text("University of Bordeaux");
+	$('.university p').text('Snapchat filter realized on Photoshop for the University of Bordeaux (Bordeaux, France) ');
+
+	$('.lhay h1').text("L'Haÿ les Roses");
+	$('.lhay p').text('Snapchat filter realized on Photoshop for the city of Parisian Region l\' Haÿ les Roses(L\'Haÿ les Roses, France) ');
+
+	$('.stmartial h1').text("Saint Martial d'Artenset ");
+	$('.stmartial p').text('Snapchat filter realized on Photoshop for my home village, Saint Martial d\'Artenset (St Martial d\'Artenset, France) ');
+
+	}
+	else if(langue == 'FR') {
+
+			// NAVBAR
+	$('#about').text('A propos');
+	$('#creas').text('Travaux et réalisations');
+	$('#contact').text('Contact me');
+	$('#translate').text('EN');
+
+
+	// HEADER
+
+	$('.big-title h4').text('DEVELOPPEMENT FRONT END • CREA. GRAPHIQUE');
+
+	//A PROPOS
+
+	$('.container-qui h4').html('<h4> Je m\'appelle <span class="myname">Thomas Beaupertuis</span>, passionné par le web depuis quelques années déjà. L\'intégration et la création graphique sont ce que je maîtrise le mieux, et ce qui me plaît. J\'apprécie les choses simples, d\'où le design sobre de cet espace.<br><br>De <span class="tlse">Toulouse</span> à <span class="bdx">Bordeaux</span>, j\'ai acquis certaines compétences et réalisé certains projets. En dehors du web, j\'aime beaucoup la photographie, le sport et le skateboard. <b>Scroll</b> pour en savoir un peu plus.</h4>')
+
+	// CREAS
+
+	$('#sitewebs').text('Sites web');
+	$('#creagraphs').text('Créations en tout genre');
+
+
+
+	// Snapchat
+
+	$('.container-f-snap-head h1').text('Filtres Snapchat');
+	$('.container-f-snap-head p').html('<p>J\'ai réalisé des filtres Snapchat afin de m\'améliorer sous Photoshop tout en incrémentant le contenu de la communauté de ce réseau social. <br>Cela me permet également de faire connaître certaines de mes créations.</p>')
+
+	$('.colonne h1').text("Colonne aux Girondins");
+	$('.colonne p').text('Filtre Snapchat réalisé sous Photoshop pour le monument "Colonne des Girondins". (Bordeaux, France) ');
+
+	$('.miroir h1').text("Le miroir d'eau");
+	$('.miroir p').text('Filtre Snapchat réalisé sous Photoshop pour le lieu très fréquenté par les touristes et les habitants, le miroir d\'eau. (Bordeaux, France)');
+
+	$('.quais h1').text("Quais de Bordeaux");
+	$('.quais p').text('Filtre Snapchat réalisé sous Photoshop pour les quais de Bordeaux, lieu très fréquenté. (Bordeaux, France)');
+
+	$('.university h1').text("Université de Bordeaux");
+	$('.university p').text('Filtre Snapchat réalisé sous Photoshop pour l\'Université de Bordeaux. (Bordeaux, France)');
+
+	$('.lhay h1').text("L'Haÿ les Roses");
+	$('.lhay p').text('Filtre Snapchat réalisé sous Photoshop pour la ville de région Parisienne l\'Haÿ les roses. (L\'Haÿ les Roses, France)');
+
+	$('.stmartial h1').text("Saint Martial d'Artenset");
+	$('.stmartial p').text('Filtre Snapchat réalisé sous Photoshop pour mon village d\'origine, Saint Martial d\'Artenset. (St Martial d\'Artenset, France)');
+
+	}
+
+});
+
+})
+
